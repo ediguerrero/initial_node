8ebf2969114eeea553e9cf9eee42f4fa03aeb4bd
edi <edixon_guerrero96@hotmail.com>
2019-06-27 16:33:28 -0500

    Add files via upload

---
+++ node/index1.js
@@ -0,0 +1,22 @@
+function add(x1, x2){
+return x1+x2;
+
+}
+function restar(x1, x2){
+    return x1-x2;
+}
+function milti(x1, x2){
+    return x1*x2;
+}
+function divi(x1 , x2){
+if (x2 == 0){
+console.log('no se puede');
+}else {
+return x1/x2;
+
+}
+
+
+}
+
+console.log(divi(24, 0));
